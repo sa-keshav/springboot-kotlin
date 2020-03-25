5c4935fbb68eab8c15a9826156d8a9fd304521de
samira.keshavarz <samira.keshavarz@rewe-digital.com>
2020-03-25 17:07:37 +0100

    "mob next [ci-skip]"

--- src/main/kotlin/com/spring/tutorial/CustomerRepository.kt
+++ src/main/kotlin/com/spring/tutorial/CustomerRepository.kt
@@ -1,9 +1,15 @@
 package com.spring.tutorial
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Lock
+import org.springframework.data.jpa.repository.QueryHints
 import org.springframework.stereotype.Repository
+import javax.persistence.LockModeType
+import javax.persistence.QueryHint
 
 @Repository
 interface CustomerRepository: JpaRepository<Customer, Long> {
-    fun findById()
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @QueryHints(QueryHint(name = "javax.persistence.lock.timeout", value = "1000"))
+    fun findById(): Customer?
 }
